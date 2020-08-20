783476e2dc22e5280750196082efee99e5d61333
Mona Lisa <email@example.com>
2020-08-20 09:29:29 -0600

    message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Coding is probably going to kick my butt. But I will prevail!");
+        }
+    }
+}
